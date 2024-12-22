2ebde839512d0bd181a23c5205a48ae2dae0cad3
Eirik Mo <eirik@mowebdev.com>
2024-12-22 16:47:58 +0100

    feat: add gd and gD mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,3 +5,10 @@ map("n", "<C-s>", "<cmd> w <CR>")
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>") -- TODO: Exchange for betterjj
 map("n", "<C-c>", "<cmd> %y+ <CR>") -- copy whole filecontent
+
+map("n", "gD", function()
+	vim.lsp.buf.declaration()
+end, { desc = "Declaration of current symbol" })
+map("n", "gd", function()
+	vim.lsp.buf.definition()
+end, { desc = "Definition of current symbol" })
